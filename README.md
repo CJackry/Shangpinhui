7614f494e368e0076f9e5a42281161cd90e955b3
CJack <cjf1998@foxmail.com>
2022-10-04 17:19:37 +0800

    Home组件拆分

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# miyocommunity
+# Shangpinhui
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import Vue from 'vue'
 import App from './App.vue'
 //引入路由
 import router from "@/router";
+//引入三级联动组件
+import TypeNav from "@/pages/Home/TypeNav";
+// 注册全局组件
+Vue.component(TypeNav.name, TypeNav);
 
 new Vue({
     render: h => h(App),
